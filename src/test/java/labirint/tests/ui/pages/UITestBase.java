734b4fdb8a1782b441c16e4464d3f738a3c2ad7f
TimeQA <qaeducation2022@gmail.com>
2022-11-26 16:47:46 +0300

    change UITestBase

--- src/test/java/labirint/tests/ui/pages/UITestBase.java
+++ src/test/java/labirint/tests/ui/pages/UITestBase.java
@@ -7,6 +7,5 @@ import labirint.tests.ui.pages.*;
 public class UITestBase extends TestBase {
 
     protected final MainPage mainPage = new MainPage();
-    protected final SearchPage searchPage = new SearchPage();
-    protected final BasketPage basketPage = new BasketPage();
+
 }
